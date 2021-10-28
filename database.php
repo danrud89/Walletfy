737074f6dd283bebb4316e53f26c6d6023a4ae99
Daniel <d.rudnik@vp.pl>
2021-10-28 18:29:16 +0200

    feature/modify_PDOoptions_in_database_file

--- database.php
+++ database.php
@@ -1,16 +1,19 @@
 <?php
 
 $config = require_once 'config.php';
+$pdoOptions = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false
+);
 
 try {
 	
-	$db = new PDO("mysql:host={$config['host']};dbname={$config['database']};charset=utf8", $config['user'], $config['password']);
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db = new PDO("mysql:host={$config['host']};dbname={$config['database']};charset=utf8",$config['user'], 
+	$config['password'], $pdoOptions);
 
 } catch (PDOException $error) {
-	
 	echo $error->getMessage();
 	exit('Internal server error ! Please try again later.');
-	
 }
 
+?>
